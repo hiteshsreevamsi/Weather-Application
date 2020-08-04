f0e92aa11e4ee5d5da760a4426a29b0e9d25aa7a
hiteshsreevamsi <hiteshsreevamsis@gmail.com>
2020-08-04 13:30:12 +0530

    removed API Key

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 async function fetchForecast(cityName, error_func) {
-  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&mode=json&appid=0a2710521ad65fe5bd538bb18b906851&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&mode=json&appid={Your Api Key}&units=metric`;
   try {
     const result = await axios.get(url);
     error_func(true);

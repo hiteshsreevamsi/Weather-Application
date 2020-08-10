978b545d335e399b419127da1aa8194a4e2744a4
hiteshsreevamsi <56996042+hiteshsreevamsi@users.noreply.github.com>
2020-08-10 23:49:24 +0530

    added api key

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 async function fetchForecast(cityName, error_func) {
-  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&mode=json&appid={Your Api Key}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&mode=json&appid=717d353dc5c46296480cd9fe145e3503&units=metric`;
   try {
     const result = await axios.get(url);
     error_func(true);
